248c9c1865bd62b835f7284a86156347a286759e
Marc Oliva <ing.oliva@hotmail.com>
2017-11-16 21:24:46 -0500

    Project created and add dependencies

---
+++ app/src/main/java/com/fcm/pushnotifications/marcoliva/pushnotificationfcm/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.fcm.pushnotifications.marcoliva.pushnotificationfcm
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
